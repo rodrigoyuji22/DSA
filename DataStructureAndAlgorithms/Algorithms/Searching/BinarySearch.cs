cd0620c6e28e4b0360eba78e3bdb820af02c239e
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-07-16 20:59:16 -0300

    binary search + exponential search

---
+++ DataStructureAndAlgorithms/Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructureAndAlgorithms.Algorithms.Searching;
+
+public class BinarySearch
+{
+    public static int Search(int[] arr, int target, int low, int high)
+    {
+        while (low <= high)
+        {
+            var middle = (low + high) / 2;
+            if(arr[middle] == target)
+                return middle;
+            if (arr[middle] < target)
+            {
+                high = middle - 1;
+            }
+            else
+            {
+                low = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ DataStructureAndAlgorithms/Algorithms/Searching/ExponentialSearch.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructureAndAlgorithms.Algorithms.Searching;
+
+public class ExponentialSearch
+{
+    public int Search(int[] arr, int target)
+    {
+        if (arr[0] == target)
+            return 0;
+        int i = 1;
+        while (arr[i] < target && i < arr.Length)
+            i = i * 2;
+        if (arr[i] == target)
+            return i;
+        return BinarySearch.Search(arr, target, i /2, Math.Min(i, arr.Length - 1));
+    }
+}
