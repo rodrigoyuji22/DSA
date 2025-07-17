d18206403cba244a7af98768ad1a5ad2f0ae6d76
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-07-16 21:13:58 -0300

    selection sort

---
+++ DataStructureAndAlgorithms/Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,22 @@
+﻿namespace DataStructureAndAlgorithms.Algorithms.Sorting;
+
+public class SelectionSort
+{
+    public static void Sort(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            if (minIndex != i)
+            {
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+                // tuple swaping instead of using a var aux to change the values
+            }
+        }
+    }
+}
