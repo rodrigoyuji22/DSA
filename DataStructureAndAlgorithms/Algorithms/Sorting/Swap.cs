142790d0b7e4b9a1605be599bc3d9e0230dd97e7
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-07-17 01:00:40 -0300

    swap method

---
+++ DataStructureAndAlgorithms/Algorithms/Sorting/Swap.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructureAndAlgorithms.Algorithms.Sorting;
+
+public class Swap
+{
+    public static void __swap(int a, int b)
+    // using an aux var
+    {
+        var aux = a;
+        a = b;
+        b = aux;
+    }
+    
+
+    public void _swap(int a, int b)
+    // tuple swaping
+    {
+        (a, b) = (b, a);
+    }
+}
