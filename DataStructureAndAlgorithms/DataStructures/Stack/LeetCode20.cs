65880c0a06934d83a6400607442ee4bb32dcc521
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-08-24 21:32:42 -0300

    .

---
+++ DataStructureAndAlgorithms/DataStructures/Stack/LeetCode20.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructureAndAlgorithms.DataStructures.Stack;
+
+public class LeetCode20
+{
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if(c == '(' || c == '[' || c == '{' )
+                stack.Push(c);
+            else
+            {
+                if(stack.Count == 0)
+                    return false;
+                var compare = stack.Pop();
+                if((c == ')' && compare != '(' ) ||  
+                   (c == ']' && compare != '[')  ||  
+                   (c == '}' && compare != '{'))
+                {
+                    return false;
+                }
+            }
+        }
+        if (stack.Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
