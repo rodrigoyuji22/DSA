e0de1590eaf35f94a11845b0d18f4af3423ecb30
Rodrigo <rodrigoyujik@gmail.com>
2025-09-08 19:33:18 -0300

    .

---
+++ DataStructureAndAlgorithms/DataStructures/Matrizes/manipular_matriz.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructureAndAlgorithms.DataStructures.Matrizes;
+
+public class manipular_matriz
+{
+    int [,] array2d = new int [3,4];
+    /*
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0};
+    */
+    int[,] _array2d = new int[,]
+    {
+        { 1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 }
+    };
+    // percorrer array
+    public void Loop()
+    {
+        for (int i = 0; i < _array2d.GetLength(0); i++)
+        {
+            for (int j = 0; j < _array2d.GetLength(1); j++)
+            {
+                Console.WriteLine(_array2d[i,j]);
+            }
+        }
+        // acessa os elementos da matriz como se fosse por um plano ortogonal, complexidade temporal O(N*M)
+    }
+}
+
