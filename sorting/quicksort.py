926d12b7710191c48f84396150d841d0a380f07e
Rodrigo Koike <rodrigoyujikoike@gmail.com>
2025-12-20 22:55:28 -0300

    fix: identação

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -11,7 +11,7 @@ def partition(arr, left, right):
         if arr[j] < pivot:
             i +=1
             arr[j], arr[i] = arr[i], arr[j]
-        arr[i+1], arr[right] = arr[right], arr[i+1]
+    arr[i+1], arr[right] = arr[right], arr[i+1]
     return i+1
     
     
