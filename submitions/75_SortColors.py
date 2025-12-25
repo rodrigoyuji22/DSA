89c51a5207ca4cedc27cf22a3a6dfd5806fa7e15
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-12-25 10:10:49 -0300

    submition .75 Sort Colors

---
+++ submitions/75_SortColors.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(len(nums)-1):
+            lowest = i
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[lowest]:
+                    lowest = j
+            nums[i], nums[lowest] = nums[lowest], nums[i]
