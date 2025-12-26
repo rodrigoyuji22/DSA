66a06aa3ccbd132cb0d7a21c8a39c6478a10766d
Rodrigo Koike <rodrigoyujikoike@gmail.com>
2025-12-26 16:38:46 -0300

    implementação binary tree

---
+++ algorithms/binary_trees_traversals.py
@@ -0,0 +1,41 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, val):
+        if not self.root:
+            self.root = TreeNode(val)
+        else:
+            self._insert_recursive(self.root, val)
+    
+    def _insert_recursive(self, node, val):
+        if val < node.val:
+            if node.left:
+                self._insert_recursive(node.left, val)
+            else:
+                node.left = TreeNode(val)
+        else:
+            if node.right:
+                self._insert_recursive(node.right, val)
+            else:
+                node.right = TreeNode(val)
+                
+    def search(self, val):
+        return self._search_recursive(self.root, val)
+        
+    def _search_recursive(self, node, val):
+        if not node:
+            return False
+        if node.val == val:
+            return True
+        if val < node.val:
+            self._search_recursive(node.left, val)
+        self._search_recursive(node.right, val)
+                
+        
